c305d722901e7260aab602dcc60c1cdecdc8cafb
gweingil <gweingil@gmail.com>
2021-09-28 22:16:02 +0600

    created new files

--- task_6.py
+++
@@ -1,7 +0,0 @@
-distance = float(input('Дистанция в первый день: '))
-goal = float(input('Конечная цель: '))
-days = 1
-while distance < goal:
-    distance *= 1.1
-    days += 1
-print(f'До цели нужно: {days} дней ')
